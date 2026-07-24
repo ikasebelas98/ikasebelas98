db4dc4afb4422bfb3629b65faf177463ae9ffaea
ikasebelas98 <ika.sebelas.98@gmail.com>
2026-07-24 15:43:28 +0700

    tes

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,12 +1,26 @@
 console.log("navbar.js loaded");
+const navItems = document.querySelectorAll(".nav-item");
+
+console.log(navItems);
 
 const navItem = document.querySelector("#tentang-kita");
+const tentangKita = document.querySelector("#tentang-kita");
+const alumni = document.querySelector("#alumni-center");
+const media = document.querySelector("#media-center");
+const community = document.querySelector("#community-hub");
+const donasi = document.querySelector("#donasi");
+const login = document.querySelector("#login");
 
-if (navItem) {
+if (tentangKita) {
 
     console.log("Tentang Kita ditemukan");
+    console.log(alumni);
+    console.log(media);
+    console.log(community);
+    console.log(donasi);
+    console.log(login);
 
-    navItem.addEventListener("mouseenter", () => {
+    tentangKita.addEventListener("mouseenter", () => {
 
         console.log("OPEN");
 
@@ -14,7 +28,7 @@ if (navItem) {
 
     });
 
-    navItem.addEventListener("mouseleave", () => {
+    tentangKita.addEventListener("mouseleave", () => {
 
         console.log("CLOSE");
 
